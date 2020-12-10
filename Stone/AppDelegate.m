f2203647b5ef90324d45951a5cc035d3c84bbe99
Ryan Baumbach <github@ryan.codes>
2020-12-09 19:36:10 -0700

    Add initial project

---
+++ Stone/AppDelegate.m
@@ -0,0 +1,9 @@
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification { }
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification { }
+
+@end
